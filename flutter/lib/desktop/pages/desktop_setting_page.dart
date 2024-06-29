e163dea462c6a3d00a0af82dc65a4c4e5d5fa900
xDoctors <zi.yan@live.cn>
2024-06-30 00:55:34 +0800

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -1764,12 +1764,6 @@ class _AboutState extends State<_About> {
                   SelectionArea(
                       child: Text('${translate('Version')}: $version')
                           .marginSymmetric(vertical: 4.0)),
-                  SelectionArea(
-                      child: Text('${translate('Build Date')}: $buildDate')
-                          .marginSymmetric(vertical: 4.0)),
-                  SelectionArea(
-                      child: Text('${translate('Fingerprint')}: $fingerprint')
-                          .marginSymmetric(vertical: 4.0)),
                   InkWell(
                       onTap: () {
                         launchUrlString('http://xintechs.com');
