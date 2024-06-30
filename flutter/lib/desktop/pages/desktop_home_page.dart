61693ea8a81664a044ee957a274c6de5458edb1b
xDoctors <zi.yan@live.cn>
2024-06-30 18:32:56 +0800

    Update desktop_home_page.dart 注释安装提示

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -86,6 +86,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
       buildTip(context),
       if (!isOutgoingOnly) buildIDBoard(context),
       if (!isOutgoingOnly) buildPasswordBoard(context),
+      /*
       FutureBuilder<Widget>(
         future: buildHelpCards(),
         builder: (_, data) {
@@ -103,6 +104,7 @@ class _DesktopHomePageState extends State<DesktopHomePage>
           }
         },
       ),
+      */
       buildPluginEntry(),
     ];
     if (isIncomingOnly) {
